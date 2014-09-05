2baafa0882f6984a952fde15ce7c34daf0f52c4e
Alessandro Dias <ale.dias@hotmail.com.br>
2014-09-04 22:28:41 -0300

    ADD: Read values from array and loop through it;

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ email-marketing-parser
 ======================
 
 Parse links on an E-mail Marketing html file, replacing variables with values.
+
+It changes this: <a href="#link_name#">Google</a> to this <a href="http://www.google.com.br&search=%s">Google</a>
--- test/main.js
+++ test/main.js
@@ -1,30 +1,24 @@
-var fs = require('fs');
+var fs = require('fs'),
+	html_file = __dirname + '/index.html';
 
-var html_file = __dirname + '/index.html';
-
-// ToDo: read links from a json file
-// var links_to_replace = require('./links.json');
-
-var link_topo = "http://www.novahaus.com.br",
-	link_corpo = "http://www.google.com.br&search=%s",
-	link_rodape = "http://www.amazon.com.br",
-	link_facebook = "http://www.facebook.com",
-	link_twitter = "http://www.twitter.com";
+// Put your variables and values here !!
+var links_to_replace = {
+	link_topo : 'http://www.novahaus.com.br',
+	link_corpo : 'http://www.google.com.br&search:%s',
+	link_rodape : 'http://www.amazon.com.br',
+	link_facebook : 'http://www.facebook.com',
+	link_twitter : 'http://www.twitter.com'
+}
 
 fs.readFile(html_file, {encoding: 'utf-8'}, function (err, data) {
 	if (err) { throw err; }
 
-	data = data.replace('#link_topo#', link_topo);
-	data = data.replace('#link_corpo#', link_corpo);
-	data = data.replace('#link_rodape#', link_rodape);
-	data = data.replace('#link_facebook#', link_facebook);
-	data = data.replace('#link_twitter#', link_twitter);
+	// Loop over array, replacing key by value
+	for(var i in links_to_replace) {
+		data = data.replace('#' + i + '#', links_to_replace[i]);
+	}
 
-	// ToDo: Loop over it, replacing variables with the values of the json file
-	// for () {
-		// ...
-	//}
-
+	// Updates the html file
 	fs.writeFile(html_file, data, {encoding: 'utf-8'}, function (err) {
 		if (err) return console.log(err);
 		console.log('\n File Parsed with Success!! \n \n Saved at: ' + html_file );
